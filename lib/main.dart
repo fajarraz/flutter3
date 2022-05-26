690c5fe2e582a12514b9c3f73ed974da23ed284e
farahis92 <fajarraz70@gmail.com>
2022-05-26 15:02:19 +0700

    flutter 3

--- lib/main.dart
+++ lib/main.dart
@@ -38,7 +38,7 @@ class HomePage extends StatelessWidget {
         onPressed: () {
           Navigator.of(context).push(MaterialPageRoute(builder: (context) => const About()));
         },
-        child: Text("Go Profile"),
+        child: Text("Go Profiles"),
       ),
     );
   }
